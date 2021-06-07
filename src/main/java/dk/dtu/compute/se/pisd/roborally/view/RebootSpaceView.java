fdd6ddc7156594bb2fa7b5a725fa69ac2a20d3d0
Andreas Carlsen <andreas.carlsen98@gmail.com>
2021-06-07 14:40:45 +0200

    Added RebootSpaceView, and placed temporary code

---
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/view/RebootSpaceView.java
@@ -0,0 +1,43 @@
+package dk.dtu.compute.se.pisd.roborally.view;
+
+import dk.dtu.compute.se.pisd.roborally.controller.FieldAction;
+import dk.dtu.compute.se.pisd.roborally.controller.RebootSpace;
+import dk.dtu.compute.se.pisd.roborally.controller.RotatingGear;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+
+public class RebootSpaceView {
+
+    public static void drawRebootSpace(SpaceView spaceView, FieldAction action) {
+        Canvas canvas = new Canvas(SpaceView.SPACE_WIDTH, SpaceView.SPACE_HEIGHT);
+        GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
+
+
+        graphicsContext.setStroke(Color.LIME);
+        graphicsContext.setLineWidth(3.);
+        double[] xList = new double[]{42.5, 32.5};
+        double[] yList = new double[]{62.5, 62.5};
+
+        graphicsContext.strokePolygon(xList, yList, xList.length);
+
+        RebootSpace rotatingGear = (RebootSpace) action;
+
+        // Increase font size from standard.
+        Font font = new Font(10);
+        graphicsContext.setFont(font);
+
+        // Draw larger shadow of the number.
+        graphicsContext.setStroke(Color.RED);
+        graphicsContext.setLineWidth(4.);
+        graphicsContext.strokeText("RebootSpace", 8, 43);
+
+        // Draw the number over the shadow.
+        graphicsContext.setStroke(Color.GREEN);
+        graphicsContext.setLineWidth(2.);
+        graphicsContext.strokeText("RebootSpace", 8, 43);
+
+        spaceView.getChildren().add(canvas);
+    }
+}
