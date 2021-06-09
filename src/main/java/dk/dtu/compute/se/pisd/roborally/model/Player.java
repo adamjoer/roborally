48ab1c61fd133f2bf3611f1fb084bda2ae242921
Andreas Carlsen <andreas.carlsen98@gmail.com>
2021-06-09 10:51:57 +0200

    Changed number of programming cards players get

--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/Player.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/Player.java
@@ -39,7 +39,7 @@ public class Player extends Subject {
 
     final public static int NO_REGISTERS = 5;
     final public static int NO_CARDS = 8;
-    final public static int NO_PROGRAM_CARDS = 8;
+    final public static int NO_PROGRAM_CARDS = 20;
 
     final public Board board;
 
