3935c9b937a76ecef02d23432a46830ebc146fc4
Adam <adamharald440@gmail.com>
2021-06-11 23:05:49 +0200

    Board: Omg go tf away, import statements

--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/Board.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/Board.java
@@ -22,8 +22,6 @@
 package dk.dtu.compute.se.pisd.roborally.model;
 
 import dk.dtu.compute.se.pisd.designpatterns.observer.Subject;
-import dk.dtu.compute.se.pisd.roborally.controller.FieldAction;
-import dk.dtu.compute.se.pisd.roborally.controller.PitfallSpace;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
